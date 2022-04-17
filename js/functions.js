3f51aa91275cb2b2d3a14b73da002976d0190102
José Velásquez <67164483+DarthSeveros@users.noreply.github.com>
2022-04-17 14:08:18 -0400

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -9,7 +9,7 @@ function setTime(){
         day -= 30;
     }
     document.getElementById("days").innerHTML = (20 - day) + "<h3>Días</h3>";
-    document.getElementById("hours").innerHTML = (13 - hour) + "<h3>Horas</h3>";
+    document.getElementById("hours").innerHTML = (14 - hour) + "<h3>Horas</h3>";
     document.getElementById("minutes").innerHTML = (60 - min) + "<h3>Minutos</h3>";
     document.getElementById("seconds").innerHTML = (60 - sec) + "<h3>Segundos</h3>";
     setTimeout(function () { setTime()}, 500);
